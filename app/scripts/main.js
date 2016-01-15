ab259a6cfa27b8632f58c3124668b8d1958218fd
Liron Shimrony <lsxliron@gmail.com>
2016-01-15 15:33:50 -0500

    fixed scrolling buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,23 +12,23 @@ require.config({
 
 require(['jquery'], function($){
 
-	$('#welcomePage button').on('click', function(){
+	$('#welcomePage button').bind('click', function(){
 		$('body').animate({scrollTop: $('#aboutMePage').offset().top}, 1000);
 	});
 
-	$('#aboutMePage button').on('click', function(){
+	$('#aboutMePage button').bind('click', function(){
 		$('body').animate({scrollTop: $('#portfolioPage').offset().top}, 1000);
 	});
 
-	$('#portfolioPage button').on('click', function(){
+	$('#portfolioPage button').bind('click', function(){
 		$('body').animate({scrollTop: $('#programmingProjects').offset().top}, 1000);
 	});
 
-	$('#programmingProjects button').on('click', function(){
+	$('#programmingProjects button').bind('click', function(){
 		$('body').animate({scrollTop: $('#contactMePage').offset().top}, 1000);
 	});
 
-	$('#contactMePage button').on('click', function(){
+	$('#contactMePage button').bind('click', function(){
 		$('body').animate({scrollTop: $('#profilesPage').offset().top}, 1000);
 	});
 
